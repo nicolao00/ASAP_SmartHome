d70f1c40fbc90e169f461e04261b24f38ebd3185
nicolao00 <nicolao00@naver.com>
2025-06-18 18:10:26 +0900

    fix: DB 정보 삭제

--- src/main/java/asap/demo/repository/FireDetectionEventRepository.java
+++
@@ -1,7 +0,0 @@
-package asap.demo.repository;
-
-import asap.demo.entity.FireDetectionEvent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FireDetectionEventRepository extends JpaRepository<FireDetectionEvent, Long> {
-} 
--- src/main/java/asap/demo/service/FireDetectionService.java
+++ src/main/java/asap/demo/service/FireDetectionService.java
@@ -1,37 +1,23 @@
 package asap.demo.service;
 
-import asap.demo.entity.FireDetectionEvent;
-import asap.demo.entity.User;
-import asap.demo.repository.FireDetectionEventRepository;
-import asap.demo.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class FireDetectionService {
-    private final UserRepository userRepository;
-    private final FireDetectionEventRepository fireDetectionEventRepository;
     private final SMSService smsService;
 
-    @Transactional
     public void handleFireDetection() {
         log.info("화재 감지 이벤트 처리 시작");
         
-        // 이벤트 저장
-        FireDetectionEvent event = new FireDetectionEvent();
-        event.setDetectedAt(LocalDateTime.now());
-        fireDetectionEventRepository.save(event);
-        
-        log.info("화재 감지 이벤트가 발생했습니다. 감지 시간: {}", 
-            event.getDetectedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        log.info("화재 감지 이벤트가 발생했습니다. 감지 시간: {}",
+                LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
         
         log.info("화재 감지 이벤트 처리 완료");
     }
